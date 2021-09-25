7e335aff08172d74b712b30f1ac0d393ef0c6f76
heber <heber.acostalipera@gmail.com>
2021-09-24 22:33:25 -0300

    agrego useEffect y rutas con vistas y componentes

--- src/App.js
+++ src/App.js
@@ -1,71 +1,28 @@
-import React, { useState } from "react";
-
-// Material UI
-import { makeStyles } from "@material-ui/core/styles";
-
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 // Styles
 import "./App.css";
-import BotonGrande from "./components/BotonGrande";
 
-const url = "https://dog.ceo/api/breeds/list/all";
-
-const useStyles = makeStyles(theme => ({
-  button: {
-    fontSize: 20,
-    margin: theme.spacing(2, 0)
-  }
-}));
+// Components
+import Header from "./components/Header";
+import Home from "./views/Home";
+import Perrito from "./views/Perrito";
 
 function App() {
-  const classes = useStyles();
-
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [image, setImage] = useState(null);
-
-  const getData = () => {
-    setLoading(true);
-    fetch(url)
-      .then(r => r.json())
-      .then(result => {
-        setData(result);
-        setLoading(false);
-      });
-  };
-
-  const getImagenes = raza => {
-    fetch(`https://dog.ceo/api/breed/${raza}/images/random`)
-      .then(r => r.json())
-      .then(result => {
-        setImage(result);
-      });
-  };
-
   return (
-    <div className="App">
-      <div className="header">
-        <BotonGrande
-          disabled={loading || data}
-          onClick={getData}
-          className={classes.button}
-          color="primary"
-        >
-          DESCARGAR
-        </BotonGrande>
-      </div>
-      {image && <img src={image.message} width={500} alt="" />}
-      <ul>
-        {data &&
-          data.message &&
-          Object.keys(data.message).map(item => (
-            <li key={item}>
-              <BotonGrande onClick={() => getImagenes(item)}>{item}</BotonGrande>
-            </li>
-          ))}
-      </ul>
-      {loading && <div>cargando...</div>}
-    </div>
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/perrito/:breed">
+          <Perrito />
+        </Route>
+        <Route path="*">404</Route>
+      </Switch>
+    </Router>
   );
 }
 
